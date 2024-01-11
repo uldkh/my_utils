07d612d510f4960f728836d8f2dd7186d103c3eb
uldkh <96343593+uldkh@users.noreply.github.com>
2024-01-11 21:58:55 +0100

    Update connect_git.py

--- connect_git.py
+++ connect_git.py
@@ -1,10 +1,4 @@
 def github_auth(persistent_key: bool):
-  """
-  Authenticate with GitHub to access private repo. This function will 
-  detect if there is `id_ed25519` key SSH profile. If not, it will create
-  one. 
-  - `persistent_key`: Store private key in Google Drive.
-  """
   import os
 
   os.system("mkdir -p ~/.ssh")
@@ -35,16 +29,12 @@ def github_auth(persistent_key: bool):
   with open(os.path.expanduser(public_key_path), "r") as f:
     public_key = f.read()
     if fresh_key:
-      print("Please go to https://github.com/settings/ssh/new to upload the following key: ")
+      pass
     else:
-      print("Looks that a private key is already created. If you have already push it to github, no action required."
-      "\n Otherwise, Please go to https://github.com/settings/ssh/new to upload the following key: ")
+      pass
 
-    print(public_key)
 
   # add github to known hosts (you may hardcode it to prevent MITM attacks)
   os.system("ssh-keyscan -t ed25519 github.com >> ~/.ssh/known_hosts")
 
   os.system("chmod go-rwx ~/.ssh/id_ed25519")
-
-  print("Please use SSH method to clone repo.")
